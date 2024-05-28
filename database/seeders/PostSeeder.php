26f01ed7aef24a6ea4a735e5269cb33f317271cb
Jhon1606 <barrosjhon936@gmail.com>
2024-05-28 14:21:16 -0500

    Subiendo la carpetas posts de storage

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -17,16 +17,16 @@ class PostSeeder extends Seeder
         // Creamos una variable llamada post y guardamos los 100 post creados con el factory
         $posts = Post::factory(300)->create();
 
-        // Luego hacemos un foreach, llamamos a el modelo image y luego el metodo factory para crear una imagen,
+        // Luego hacemos un foreach, llamamos a el modelo image y luego el método factory para crear una imagen,
         // y le pasamos el id y el type recorriendo el arreglo
-        foreach($posts as $post){
+        foreach ($posts as $post) {
             Image::factory(1)->create([
                 'imageable_id' => $post->id,
                 'imageable_type' => Post::class
             ]);
-            // Para hacer la relacion entre los posts y los tags
-            // Llamamos a la variable post y accedemos a el metodo tags que se creo en el modelo Post, 
-            // la relacion muchos a muchos
+            // Para hacer la relación entre los posts y los tags
+            // Llamamos a la variable post y accedemos a el método tags que se creo en el modelo Post, 
+            // la relación muchos a muchos
             $post->tags()->attach([
                 rand(1, 4),
                 rand(5, 8)
